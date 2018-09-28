19593bf45d2fe386a9c7db2a275eff85c88ac46f
deftnesssolutions <gusove@gmail.com>
2018-09-27 22:54:36 -0300

    Spring boot Web
    Projeto de criaçaõ Web com Spring Boot

---
+++ eventosweb/src/main/java/com/eventosweb/eventosweb/EventoswebApplication.java
@@ -0,0 +1,12 @@
+package com.eventosweb.eventosweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventoswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventoswebApplication.class, args);
+	}
+}
